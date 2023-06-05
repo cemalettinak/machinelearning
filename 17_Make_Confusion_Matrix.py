c7f9d6fb17a3f35926c1874711a0d7a14512e0d5
cemalettinak <135587443+cemalettinak@users.noreply.github.com>
2023-06-05 14:53:26 +0300

    17_Make_Confusion_Matrix.py

--- 17_Make_Confusion_Matrix.py
+++ 17_Make_Confusion_Matrix.py
@@ -3,6 +3,7 @@
 Tüm grafikler için matplotlib kütüphanesinin pyplot modülü içe aktarılmaktadır. Ardından bu grafik için seaborn kütüphanesi de içe aktarılmıştır. 
 Sonrasında gerçek çıktı değeri ile o çıktı değerinin tahmini ve çapraz tabloya ait diğer özellikler crosstab fonksiyonu içerisine yazılmıştır.
 Boyut ayarlaması, başlık ve ısı haritası üzerine renklendirme ve yazı yazdırma ayarlamaları yapılarak show fonksiyonu ile grafik çıktısı üretilmiştir.
+Random Forest modeli için rf_pred, Gradient Boosting modeli için gb_pred, XGBoost modeli için xgb_pred, Desicion Tree modeli için dt_pred, Logistic Regression modeli için lr_pred, Voting modeli için voting_pred vb. şekilde değiştirerek diğer ısı haritaları oluşturulabilir.
 """
 
 
