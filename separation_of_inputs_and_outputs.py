2ccfed1d5f8d4ae3c54a470b37efb8736dd0d8a9
cemalettinak <135587443+cemalettinak@users.noreply.github.com>
2023-06-05 10:42:46 +0300

    separation_of_inputs_and_outputs.py
    # Define dependent and independent variables

---
+++ separation_of_inputs_and_outputs.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 11:27:30 2023
+
+@author: cemalettin
+"""
+
+
+# Defining dependent and independent variables
+X = data.drop(columns=["YatisGunGroup"])
+y = data["YatisGunGroup"]
