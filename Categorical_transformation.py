22f65e2ee5ab6ca020235257ee27681321559797
cemalettinak <135587443+cemalettinak@users.noreply.github.com>
2023-06-05 10:37:51 +0300

    Create Categorical_transformation.py

---
+++ Categorical_transformation.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 26 11:27:30 2023
+
+@author: cemalettin
+"""
+
+# Categorical transformation
+
+data['AdmissionType'] = data['AdmissionType'].astype('category')
